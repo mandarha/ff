9a25b970fb9a318424fd088c4eedc4b3713b2571
Mandar <111448138+mandarha@users.noreply.github.com>
2023-04-10 14:57:25 +0530

    Update Snowflake_File_Upload.py

--- Snowflake_File_Upload.py
+++ Snowflake_File_Upload.py
@@ -31,6 +31,4 @@ def run_query(query):
 run_query("USE ROLE ACCOUNTADMIN;")
 rows = run_query("SELECT * from sf_demo.sf_demo.emp_salary;")
 
-# Print results.
-for row in rows:
-    st.write(f"{row[0]} has a :{row[1]}:")
+st.table(rows)
